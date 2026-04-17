4d713291c70b1895a1d4874e558bd3a88bf2a0cf
Veronica Mendoza <vmendoza265@gmail.com>
2026-04-17 17:31:49 -0400

    fix model name

--- api/generate.js
+++ api/generate.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
     }
   };
 
-  const model = 'gemini-1.5-flash';
+  const model = 'gemini-2.0-flash';
   const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
 
   try {
